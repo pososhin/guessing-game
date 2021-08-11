8e8c669994b8241e9921849f0847b32131435227
pososhin <pososhin@gmail.com>
2021-08-11 17:37:53 +0300

    feat: resolved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,18 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() { }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.current = this.min + Math.floor((this.max - this.min + 1) / 2)
     }
-
     guess() {
-
+        return this.current;
     }
-
     lower() {
-
+        this.setRange(this.min, this.current);
     }
-
     greater() {
-
+        this.setRange(this.current, this.max);
     }
 }
-
 module.exports = GuessingGame;
